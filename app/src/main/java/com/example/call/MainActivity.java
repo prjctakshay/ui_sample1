f29e3a57890bc0f54ab3b61163f3b5037799ffc1
akshay <prjctakshay@gmail.com>
2020-06-23 12:47:08 +0530

    initial commit

---
+++ app/src/main/java/com/example/call/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.call;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import in.shadowfax.proswipebutton.ProSwipeButton;
+
+public class MainActivity extends AppCompatActivity {
+    ProSwipeButton proSwipeBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        proSwipeBtn = (ProSwipeButton) findViewById(R.id.awesome_btn);
+        proSwipeBtn.setOnSwipeListener(new ProSwipeButton.OnSwipeListener() {
+            @Override
+            public void onSwipeConfirm() {
+                // user has swiped the btn. Perform your async operation now
+  /*              new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // task success! show TICK icon in ProSwipeButton
+                        proSwipeBtn.showResultIcon(true); // false if task failed
+                    }
+                }, 2000);*/
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:9746074355"));//change the number.
+                //startActivity(callIntent);
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    //request permission from user if the app hasn't got the required permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CALL_PHONE},   //request specific permission from user
+                            10);
+                    Toast.makeText(MainActivity.this,"permission granted",Toast.LENGTH_SHORT).show();
+                    return;
+                }else {     //have got permission
+                    try{
+                        startActivity(callIntent);  //call activity and make phone call
+                    }
+                    catch (android.content.ActivityNotFoundException ex){
+                        Toast.makeText(getApplicationContext(),"yourActivity is not founded",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                Toast.makeText(MainActivity.this,"swiped",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
