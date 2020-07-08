92b7d65bda85609f304591ca74a37ab321d92c97
akshay <prjctakshay@gmail.com>
2020-07-08 19:16:25 +0530

    first commit

---
+++ app/src/main/java/com/example/call/ListViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.call;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+}
